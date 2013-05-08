5006742e5b6e1153a6aba1dfb1a93ca74d905c34
Glenn Gillen <me@glenngillen.com>
2013-05-07 22:32:52 -0700

    I don't think we want HTML to be escaped.

--- lib/hamstache/haml.rb
+++ lib/hamstache/haml.rb
@@ -4,16 +4,16 @@ module Haml
     alias_method :process_line_without_mustache, :process_line
 
     def process_line(text, index)
-      if text =~ /^\{\{.*\}\}$/        
+      if text =~ /^\{\{.*\}\}$/
         return push mustache(text)
       else
         process_line_without_mustache(text, index)
       end
     end
-    
-    def mustache(line)    
-      ParseNode.new(:script, @index, :text => %Q{\"#{line}\"}, 
-        :escape_html => true,
+
+    def mustache(line)
+      ParseNode.new(:script, @index, :text => %Q{\"#{line}\"},
+        :escape_html => false,
         :self_closing => true,
         :dont_push_end => true)
 
